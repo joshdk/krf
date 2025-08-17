8f4f0cebe86bae1f86da1d616d36058e1b7c2a96
Josh Komoroske <jdkomo@gmail.com>
2025-08-17 14:39:52 -0400

    feat: print resources as a formatted table

---
+++ printer/table.go
@@ -0,0 +1,29 @@
+// Copyright Josh Komoroske. All rights reserved.
+// Use of this source code is governed by the MIT license,
+// a copy of which can be found in the LICENSE.txt file.
+// SPDX-License-Identifier: MIT
+
+package printer
+
+import (
+	"io"
+
+	"github.com/rodaine/table"
+
+	"github.com/joshdk/krf/resources"
+)
+
+// Table prints each given resources.Resource as a row in a formatted table.
+func Table(w io.Writer, items []resources.Resource) error {
+	tbl := table.New("Namespace", "API Version", "Kind", "Name")
+	tbl.WithHeaderSeparatorRow('─')
+	tbl.WithWriter(w)
+
+	for _, item := range items {
+		tbl.AddRow(item.GetNamespace(), item.GetAPIVersion(), item.GetKind(), item.GetName())
+	}
+
+	tbl.Print()
+
+	return nil
+}
