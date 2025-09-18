1d6cee91332ab9e37968892273a64261e8c5d5f5
Josh Komoroske <jdkomo@gmail.com>
2025-09-18 08:57:52 -0400

    feat: contains matcher flag

---
+++ matcher/matcher-contains_test.go
@@ -0,0 +1,32 @@
+// Copyright Josh Komoroske. All rights reserved.
+// Use of this source code is governed by the MIT license,
+// a copy of which can be found in the LICENSE.txt file.
+// SPDX-License-Identifier: MIT
+
+package matcher_test
+
+import (
+	"testing"
+
+	"github.com/joshdk/krf/matcher"
+)
+
+func TestContainsMatcher(t *testing.T) {
+	t.Parallel()
+
+	testMatcher(t, []spec{
+		{
+			title:   "image substring",
+			matcher: matcher.NewContainsMatcher("mage: gcr.io/goog"),
+			matches: []string{"Pod/test-pod"},
+		},
+		{
+			title:   "port substring",
+			matcher: matcher.NewContainsMatcher("ort: 80"),
+			matches: []string{
+				"Deployment/nginx-deployment",
+				"Service/my-service",
+			},
+		},
+	})
+}
